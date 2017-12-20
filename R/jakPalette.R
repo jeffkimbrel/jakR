01184a6fb920356cc287d3d367f2a1c2b92ea4d3
Jeff Kimbrel <jakpot@gmail.com>
2017-12-20 13:28:27 -0800

    initial commit

---
+++ R/jakPalette.R
@@ -0,0 +1,8 @@
+#' A Color Palette Function
+#'
+#' This palette can be used all over
+#' @export
+#' @examples
+#' jakPalette
+
+jakPalette = colorRampPalette(c("#181f3b", "#2f4d6d", "#feda5c", "#c85445", "#bd8e81"))
