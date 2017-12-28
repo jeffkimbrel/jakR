66f40c8065aeb76946ea25e7671947986149642f
Jeff Kimbrel <jakpot@gmail.com>
2017-12-28 10:04:49 -0800

    Added rooted tree check to estimate_richness_full

--- R/estimateRichnessFull.R
+++ R/estimateRichnessFull.R
@@ -1,9 +1,15 @@
 #' Alpha-Diversity Measures
 #'
-#' An extension of the phyloseq `estimate_richness` function to include Faith's PD and Shannon's Evenness
+#' An extension of the phyloseq `estimate_richness` function to include Shannon's Evenness and Faith's PD (with runPD = TRUE). It also combines the normal estimate_richness output with the sample data dataframe.
+#' Running Faith's PD can add a significant amount of time if the tree is large. Faith's PD also requires a rooted tree. If the phyloseq object tree is not rooted, then this function loads the phytools package and does midpoint.root().
 #' @export
 
 estimate_richness_full = function(phyloObject, runPD = FALSE) {
+
+  require("phyloseq")
+  require("dplyr")
+
+  phyloObject = filter_taxa(phyloObject, function(x) sum(x >= 1) >= (1), TRUE)
 
   sampleData = as.data.frame(sample_data(phyloObject))
 
@@ -21,6 +27,11 @@ estimate_richness_full = function(phyloObject, runPD = FALSE) {
 
     comm = as.data.frame(t(otu_table(phyloObject)))
     tre = phy_tree(phyloObject)
+    if (!is.rooted(tre)) {
+      require("phytools")
+      tre = midpoint.root(tre)
+    }
+
     pd.results = pd(comm, tre)
     pd.results = select(pd.results, PD)
 
