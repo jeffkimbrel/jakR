a2090642343a15622124891aae842732a525738d
Jeff Kimbrel <jakpot@gmail.com>
2018-09-18 15:35:44 -0700

    v0.4.2
    Phylo diversity extravaganza
    
    Removed runPD from estimate_richness_full
    Changed estimate_richness_full to getAlphaDiversity
    Added getPhyloDiversity to calculate PD, MPD, MNTD

--- R/getAlphaDiversity.R
+++ R/getAlphaDiversity.R
@@ -4,16 +4,15 @@
 #' Running Faith's PD can add a significant amount of time if the tree is large. Faith's PD also requires a rooted tree. If the phyloseq object tree is not rooted, then this function loads the phytools package and does midpoint.root().
 #' @export
 
-estimate_richness_full = function(phyloObject, runPD = FALSE) {
+getAlphaDiversity = function(p) {
 
   require("phyloseq")
-  require("dplyr")
 
-  phyloObject = filter_taxa(phyloObject, function(x) sum(x >= 1) >= (1), TRUE)
+  p = filter_taxa(p, function(x) sum(x >= 1) >= (1), TRUE)
 
-  sampleData = data.frame(sample_data(phyloObject))
+  sampleData = data.frame(sample_data(p))
 
-  rich = estimate_richness(phyloObject)
+  rich = estimate_richness(p)
   rich$ShannonEvenness = rich$Shannon / log(rich$Observed)
   row.names(rich) = gsub("\\.", "-", row.names(rich))
   rich$SAMPLE = as.factor(row.names(rich))
@@ -21,32 +20,12 @@ estimate_richness_full = function(phyloObject, runPD = FALSE) {
   rich = merge(sampleData, rich)
   rownames(rich) = rich$SAMPLE
 
-  depth = as.data.frame(sample_sums(phyloObject))
+  depth = as.data.frame(sample_sums(p))
   depth$SAMPLE = rownames(depth)
   colnames(depth) = c("SAMPLE_SUMS", "SAMPLE")
 
   rich = merge(rich, depth)
   rownames(rich) = rich$SAMPLE
 
-  # PD
-  if (runPD == TRUE) {
-
-    require("picante")
-
-    comm = as.data.frame(t(otu_table(phyloObject)))
-    tre = phy_tree(phyloObject)
-    if (!is.rooted(tre)) {
-      require("phytools")
-      tre = midpoint.root(tre)
-    }
-
-    pd.results = pd(comm, tre)
-    pd.results = select(pd.results, PD)
-
-    pd.results$SAMPLE = as.factor(rownames(pd.results))
-    rich = left_join(rich, pd.results, by = "SAMPLE")
-  }
-
-  row.names(rich) = rich$SAMPLE
   rich
 }
---
+++ R/getPhyloDiversity.R
@@ -0,0 +1,74 @@
+#' Phylogenetic Diversity Measures
+#'
+#' @export
+
+getPhyloDiversity = function(p, runs = 999) {
+  require("picante")
+  p = filter_taxa(p, function(x) sum(x >= 1) >= 1, TRUE)
+
+  # make initial objects
+  tree = phy_tree(p)
+
+  # root tree if necessary
+  if (!is.rooted(tree)) {
+    require("phytools")
+    tree = midpoint.root(tree)
+  }
+
+  tree.co <- cophenetic(tree)
+  counts = as.data.frame(t(otu_table(p)))
+  counts <- counts[, tree$tip.label] # make sure counts is in the same order as phy.dist
+
+  # PD
+  PD = pd(counts, tree, include.root = TRUE)
+  phyloDiversity = PD %>% select(PD)
+
+  # MPD
+  MPD.unweighted = mpd(counts, tree.co, abundance.weighted = FALSE)
+  MPD.weighted   = mpd(counts, tree.co, abundance.weighted = TRUE)
+  phyloDiversity$MPD.u = MPD.unweighted
+  phyloDiversity$MPD.w = MPD.weighted
+
+  # MNTD
+  MNTD.unweighted = mntd(counts, tree.co, abundance.weighted = FALSE)
+  MNTD.weighted   = mntd(counts, tree.co, abundance.weighted = TRUE)
+  phyloDiversity$MNTD.u = MNTD.unweighted
+  phyloDiversity$MNTD.w = MNTD.weighted
+
+  # SES.MPD
+  SES.MPD.unweighted = ses.mpd(counts, tree.co, null.model = "richness", abundance.weighted = FALSE, runs = runs) %>%
+    select(mpd.obs.z, mpd.obs.p) %>%
+    rename(ses.MPD.u.z = mpd.obs.z, ses.MPD.u.p = mpd.obs.p)
+  SES.MPD.weighted   = ses.mpd(counts, tree.co, null.model = "richness", abundance.weighted = TRUE,  runs = runs) %>%
+    select(mpd.obs.z, mpd.obs.p) %>%
+    rename(ses.MPD.w.z = mpd.obs.z, ses.MPD.w.p = mpd.obs.p)
+
+  phyloDiversity = merge(phyloDiversity, SES.MPD.unweighted, by = 'row.names')
+  rownames(phyloDiversity) = phyloDiversity$Row.names
+  phyloDiversity$Row.names = NULL
+  phyloDiversity = merge(phyloDiversity, SES.MPD.weighted, by = 'row.names')
+  rownames(phyloDiversity) = phyloDiversity$Row.names
+  phyloDiversity$Row.names = NULL
+
+  # SES.MNTD
+  SES.MNTD.unweighted = ses.mntd(counts, tree.co, null.model = "richness", abundance.weighted = FALSE, runs = runs) %>%
+    select(mntd.obs.z, mntd.obs.p) %>%
+    rename(ses.MNTD.u.z = mntd.obs.z, ses.MNTD.u.p = mntd.obs.p)
+  SES.MNTD.weighted   = ses.mntd(counts, tree.co, null.model = "richness", abundance.weighted = TRUE,  runs = runs) %>%
+    select(mntd.obs.z, mntd.obs.p) %>%
+    rename(ses.MNTD.w.z = mntd.obs.z, ses.MNTD.w.p = mntd.obs.p)
+
+  phyloDiversity = merge(phyloDiversity, SES.MNTD.unweighted, by = 'row.names')
+  rownames(phyloDiversity) = phyloDiversity$Row.names
+  phyloDiversity$Row.names = NULL
+  phyloDiversity = merge(phyloDiversity, SES.MNTD.weighted, by = 'row.names')
+  rownames(phyloDiversity) = phyloDiversity$Row.names
+  phyloDiversity$Row.names = NULL
+
+  sampleData = data.frame(sample_data(p))
+  phyloDiversity = merge(sampleData, phyloDiversity, by = 'row.names')
+  rownames(phyloDiversity) = phyloDiversity$Row.names
+  phyloDiversity$Row.names = NULL
+
+  return(phyloDiversity)
+}
