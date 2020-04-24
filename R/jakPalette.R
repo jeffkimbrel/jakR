87f7a4abdfb6f9290b3730e3ef89673903b488e3
Jeff Kimbrel <jakpot@gmail.com>
2020-04-24 15:42:36 -0700

    v0.6.8

--- R/jakPalette.R
+++ R/jakPalette.R
@@ -5,4 +5,7 @@
 #' @examples
 #' jakPalette(n)
 
-jakPalette = colorRampPalette(c("#404530", "#738427", "#d1b385", "#427fc3", "#73cbf6"))
+paletteJAK = list()
+
+paletteJAK$jak = colorRampPalette(c("#404530", "#738427", "#d1b385", "#427fc3", "#73cbf6"))
+paletteJAK$host = c("Ns" = "#4DAF4A", "Ms" = "#4DAF4A", "M. salina" = "#4DAF4A", "N. salina" = "#4DAF4A", "Pt" = "#A65628", "P. tricornutum" = "#A65628")
