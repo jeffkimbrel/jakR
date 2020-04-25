f3574fe7455f67a09249c6cabc4805a98605bc40
Jeff Kimbrel <jakpot@gmail.com>
2020-04-25 10:08:26 -0700

    v0.7.0
    New pale color palette to colorbook
    incorporate speedyseq test to get_phylo_palette function... makes it run in ~2 seconds rather than ~20. Nice!
    Also made the script return all taxonomic levels rather than only a specified one.

--- R/colorbookPalette.R
+++ R/colorbookPalette.R
@@ -10,3 +10,4 @@ colorbook$helpcenter   = colorRampPalette(c('#E40035','#6900C5','#2F4AF9','#00BB
 colorbook$bulmaDefault = colorRampPalette(c('#01D1B2','#5178FA','#3273DC','#23D160','#FFDD57','#FF3860'))
 colorbook$blackDiamond = colorRampPalette(c('#6C7F50','#98C67C','#34557F','#FF635A','#D98626','#D3BF96','#685A5C','#786592','#78849C','#DC443A','#ACDEE6','#EFBD47','#B0583A','#E2C2C7','#0088CE','#E9BF9B','#652D86','#512B1B','#D1D4D3','#FE9666','#7BBBB2'))
 colorbook$sachi        = colorRampPalette(c('#005eac','#E7004C','#00C36B','#6D6462','#068CD6','#FE1E36','#F57C00','#0097A7','#A35FCA'))
+colorbook$inslife      = colorRampPalette(c('#80bec8','#6EB5C0','#e8e2e6','#FFCCBB','#83a0cd','#dee6f2','#DEAFB1','#AFDEDC','#DCC48E','#CEB28D','#56CBF9','#E9D7C3'))
---
+++ R/get_phylo_palette.R
@@ -0,0 +1,53 @@
+#' getPhyloPalette
+#'
+#' @export
+#' @param p A Phyloseq object
+#' @param n Max number of taxa to be included in the "primary" palette, with the rest in a secondary, less dramatic palette.
+
+get_phylo_palette = function (p, n = 10) {
+  require("phyloseq")
+  require("tidyverse")
+  require("speedyseq")
+
+  pal = list()
+
+  tax_levels = colnames(tax_table(p))
+
+  df = speedyseq::psmelt(p)
+
+  for (level in tax_levels) {
+    print(level)
+
+    df.level = df[, c(level, "Abundance")]
+    colnames(df.level) = c("TAXA", "ABUNDANCE")
+
+    sorted_taxa = df.level %>%
+      group_by(TAXA) %>%
+      summarise(SUM = sum(ABUNDANCE)) %>%
+      arrange(desc(SUM))
+
+    top_taxa = sorted_taxa %>%
+      top_n(n = 10, wt = SUM) %>%
+      mutate(TAXA = as.character(TAXA)) %>%
+      pull(TAXA)
+
+    bottom_taxa = sorted_taxa %>%
+      filter(!TAXA %in% top_taxa) %>%
+      mutate(TAXA = as.character(TAXA)) %>%
+      pull(TAXA)
+
+    top_palette = sample(colorbook$helpcenter(length(top_taxa)))
+    names(top_palette) = top_taxa
+
+    bottom_palette = sample(colorbook$inslife(length(bottom_taxa)))
+    names(bottom_palette) = bottom_taxa
+
+    taxa_palette = c(top_palette, bottom_palette)
+    taxa_palette["Other"] = "#555555"
+    taxa_palette["NA"] = "#000000"
+
+    pal[[level]] = taxa_palette
+  }
+  return(pal)
+}
+
