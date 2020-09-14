7b08ab70743d94d0a79a2dc82dde485bb2c39be4
Jeff Kimbrel <jakpot@gmail.com>
2020-09-14 15:09:02 -0700

    v0.8.10
    factor issue

--- R/run_fts.R
+++ R/run_fts.R
@@ -46,8 +46,8 @@ run_fts = function (ms_object, taxa_level, fts_class, fts_id, fts_time, B = 99, 
       include = c("1", "class", "time:class")
       timePoints = seq(min(dat$time), max(dat$time), by = 1)
 
-      group0 = data.frame(time = timePoints, class = levels(dat$class)[1])
-      group1 = data.frame(time = timePoints, class = levels(dat$class)[2])
+      group0 = data.frame(time = timePoints, class = as.factor(levels(dat$class)[1]))
+      group1 = data.frame(time = timePoints, class = as.factor(levels(dat$class)[2]))
       pred0 = predict(mod, newdata = group0, include = include, se = TRUE)
       pred1 = predict(mod, newdata = group1, include = include, se = TRUE)
 
