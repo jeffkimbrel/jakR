8527a3e2dd670d12a669181544fcc2020ff50d25
Jeff Kimbrel <jakpot@gmail.com>
2022-04-04 15:29:46 -0700

    v0.9.21
    fix for get_alpha_diversity to use a defined column name

--- R/getAlphaDiversity.R
+++ R/getAlphaDiversity.R
@@ -7,17 +7,18 @@
 #' The new version removes much of the phyloseq code, running them instead in Vegan or calculating manually.
 #'
 #' @param p A phyloseq object
+#' @param id_col The name of the column with sample IDs
 #'
 #' @export
 
-get_alpha_diversity = function (p) {
+get_alpha_diversity = function (p, id_col = "SAMPLE") {
   require("phyloseq")
   require("vegan")
 
   p = filter_taxa(p, function(x) sum(x >= 1) >= (1), TRUE)
 
   SAMPLE_SUMS = as.data.frame(sample_sums(p)) %>%
-    rownames_to_column("SAMPLE") %>%
+    rownames_to_column(id_col) %>%
     dplyr::rename(SAMPLE_SUM = "sample_sums(p)")
 
   S = as.data.frame(otu_table(p)) %>%
@@ -26,53 +27,53 @@ get_alpha_diversity = function (p) {
     as.data.frame() %>%
     t() %>%
     as.data.frame() %>%
-    rownames_to_column("SAMPLE") %>%
-    left_join(SAMPLE_SUMS, by = "SAMPLE")
+    rownames_to_column(id_col) %>%
+    left_join(SAMPLE_SUMS, by = id_col)
 
   SIMPSON = as.data.frame(otu_table(p)) %>%
     rownames_to_column("ASV") %>%
-    pivot_longer(cols = -ASV, names_to = "SAMPLE", values_to = "ABUNDANCE") %>%
-    group_by(SAMPLE) %>%
+    pivot_longer(cols = -ASV, names_to = id_col, values_to = "ABUNDANCE") %>%
+    group_by_at(id_col) %>%
     mutate(Pi = ABUNDANCE / sum(ABUNDANCE)) %>%
-    left_join(S, by = "SAMPLE") %>%
+    left_join(S, by = id_col) %>%
     mutate(Pi2 = Pi ^ 2) %>%
-    group_by(SAMPLE) %>%
+    group_by_at(id_col) %>%
     summarise(SIMPSON_D = sum(Pi2), .groups = 'drop') %>%
     mutate(SIMPSON_EVENNESS = 1 - SIMPSON_D, SIMPSON_INVERSE = 1 / SIMPSON_D) %>%
-    left_join(S, by = "SAMPLE")
+    left_join(S, by = id_col)
 
 
   SHANNON = as.data.frame(otu_table(p)) %>%
     rownames_to_column("ASV") %>%
-    pivot_longer(cols = -ASV, names_to = "SAMPLE", values_to = "ABUNDANCE") %>%
-    group_by(SAMPLE) %>%
+    pivot_longer(cols = -ASV, names_to = id_col, values_to = "ABUNDANCE") %>%
+    group_by_at(id_col) %>%
     mutate(Pi = ABUNDANCE / sum(ABUNDANCE)) %>%
     filter(Pi > 0) %>%
     summarise(SHANNON_H = -sum(Pi*log(Pi)), .groups = 'drop') %>%
     mutate(SHANNON_ENS = exp(SHANNON_H)) %>%
-    left_join(SIMPSON, by = "SAMPLE") %>%
+    left_join(SIMPSON, by = id_col) %>%
     mutate(SHANNON_E = SHANNON_H / log(S.obs)) %>%
-    select("SAMPLE", "SAMPLE_SUM", "S.obs", "S.chao1", "se.chao1", "S.ACE", "se.ACE", "SIMPSON_D", "SIMPSON_EVENNESS", "SIMPSON_INVERSE", "SHANNON_H", "SHANNON_ENS", "SHANNON_E")
+    select(all_of(id_col), "SAMPLE_SUM", "S.obs", "S.chao1", "se.chao1", "S.ACE", "se.ACE", "SIMPSON_D", "SIMPSON_EVENNESS", "SIMPSON_INVERSE", "SHANNON_H", "SHANNON_ENS", "SHANNON_E")
 
   data.frame(sample_data(p)) %>%
-    dplyr::rename("SAMPLE_orig" = "SAMPLE") %>%
-    rownames_to_column("SAMPLE") %>%
+    dplyr::rename("SAMPLE_orig" = id_col) %>%
+    rownames_to_column(id_col) %>%
     tibble()
 
   s = data.frame(sample_data(p))
 
-  if ("SAMPLE" %in% colnames(s)) {
+  if (id_col %in% colnames(s)) {
     s = data.frame(sample_data(p)) %>%
-      dplyr::rename("SAMPLE_orig" = "SAMPLE") %>%
-      rownames_to_column("SAMPLE") %>%
+      dplyr::rename("SAMPLE_orig" = id_col) %>%
+      rownames_to_column(id_col) %>%
       tibble()
   } else {
     s = data.frame(sample_data(p)) %>%
-      rownames_to_column("SAMPLE") %>%
+      rownames_to_column(id_col) %>%
       tibble()
   }
 
-  final = left_join(s, SHANNON, by = "SAMPLE")
+  final = left_join(s, SHANNON, by = id_col)
 
   return(final)
 }
